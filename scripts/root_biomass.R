90bb373a83f7f893169c63e6fa90db688060acfa
JulianaDioquino <julianadioquino07@gmail.com>
2026-03-28 16:03:31 -0700

    juliana made plots!

--- scripts/root_biomass.R
+++ scripts/root_biomass.R
@@ -84,11 +84,11 @@ dunn.test(root_morphology$pft_biomass, root_morphology$treatment, method = "bonf
 
 # graminoid bayesian analysis 
 prior_root_biomass <- set_prior("normal(-0.03,0.05)" , class = "Intercept")
-prior_root_biomass <- c(prior_root_biomass,set_prior("normal(0,0.02)" , class = "b" , coef  = "treatmentHeatwave"))
-prior_root_biomass <- c(prior_root_biomass,set_prior("normal(0,0.02)" , class = "b" , coef  = "treatmentExtendedseason"))
-prior_root_biomass <- c(prior_root_biomass,set_prior("normal(0.01,0.3)" , class = "sigma"))
+prior_root_biomass <- c(prior_root_biomass,set_prior("normal(0,0.03)" , class = "b" , coef  = "treatmentHeatwave"))
+prior_root_biomass <- c(prior_root_biomass,set_prior("normal(0,0.03)" , class = "b" , coef  = "treatmentExtendedseason"))
+prior_root_biomass <- c(prior_root_biomass,set_prior("normal(0.01,0.5)" , class = "sigma"))
 
-bayesian_model <- brm( pft_biomass |  trunc(lb = 0) ~   treatment , # |  trunc(lb=0) 
+bayesian_model_gram <- brm( pft_biomass |  trunc(lb = 0) ~   treatment , # |  trunc(lb=0) 
                        data = graminoid,
                        iter = 5000,
                        warmup = 1000,
@@ -97,18 +97,23 @@ bayesian_model <- brm( pft_biomass |  trunc(lb = 0) ~   treatment , # |  trunc(l
                        prior = prior_root_biomass ,
                        control = list(adapt_delta = 0.99),
                        family = gaussian(),
-                       threads = threading(3),
+                       # threads = threading(3),
                        init = 0)
-plot(bayesian_model)
+
+summary(bayesian_model_gram, prob = 0.9)
+plot(bayesian_model_gram)
+pp_check(bayesian_model_gram)
   # no difference between treatments for graminoid roots
 
-# shrub bayesian analysis
-prior_root_biomass <- set_prior("normal(-0.05,0.1)" , class = "Intercept")
-prior_root_biomass <- c(prior_root_biomass,set_prior("normal(0,0.07)" , class = "b" , coef  = "treatmentHeatwave"))
-prior_root_biomass <- c(prior_root_biomass,set_prior("normal(0,0.07)" , class = "b" , coef  = "treatmentExtendedseason"))
-prior_root_biomass <- c(prior_root_biomass,set_prior("normal(0.02,0.3)" , class = "sigma"))
 
-bayesian_model <- brm( pft_biomass |  trunc(lb=0)   ~  treatment , # |  trunc(lb=0)  # model formula
+
+# shrub bayesian analysis
+prior_root_biomass <- set_prior("normal(-0.05,0.15)" , class = "Intercept")
+prior_root_biomass <- c(prior_root_biomass,set_prior("normal(0,0.09)" , class = "b" , coef  = "treatmentHeatwave"))
+prior_root_biomass <- c(prior_root_biomass,set_prior("normal(0,0.09)" , class = "b" , coef  = "treatmentExtendedseason"))
+prior_root_biomass <- c(prior_root_biomass,set_prior("normal(0.02,0.05)" , class = "sigma"))
+
+bayesian_model_shrub <- brm( pft_biomass |  trunc(lb=0)   ~  treatment , # |  trunc(lb=0)  # model formula
                        data = shrub, # dataset
                        iter = 5000, # number of smapling iteration
                        warmup = 1000, # discarded iterations at the start
@@ -119,45 +124,54 @@ bayesian_model <- brm( pft_biomass |  trunc(lb=0)   ~  treatment , # |  trunc(lb
                        family = gaussian(), # distribution
                        #threads = threading(3), # even faster
                        init = 0) # more stable sampling
-    # difference between extended season treatment for shrub roots
 
-# new data
+summary(bayesian_model_shrub, prob = 0.9)
+plot(bayesian_model_shrub)
+pp_check(bayesian_model_shrub)
+
+### SHRUB statistical reporting with the fit models ###
 new_data <- data.table(treatment = sort(unique(shrub$treatment)))
+preds <- cbind(new_data, fitted(bayesian_model_shrub,
+                                newdata = new_data, probs = c(0.05, 0.95)))
 
-preds <- cbind(new_data, fitted(bayesian_model, newdata = new_data, probs = c(0.05,0.95)))
-
-preds_full <- data.table(fitted(bayesian_model, new_data, summary = F))
+preds_full <- data.table(fitted(bayesian_model_shrub,
+                                newdata = new_data, summary = F))
 colnames(preds_full) <- as.character(new_data$treatment)
 
 preds_delta <- preds_full[,.(`Heat wave` = `Heat wave` - Control ,
                              `Extended season` =`Extended season` - Control)]
 
-preds_full <- melt(preds_full,variable.name = "treatment",value.name = "Estimate")
+# plot set up
+preds_full <- melt(preds_full, variable.name = "treatment", value.name = "Estimate")
 preds_full$treatment <- as.factor(preds_full$treatment)
 
-preds_delta <- melt(preds_delta,variable.name = "treatment",value.name = "Estimate")
+preds_delta <- melt(preds_delta, variable.name = "treatment", value.name = "Estimate")
 
-ggplot(preds,aes(x = treatment, y = Estimate)) +
+# distribution of the mean plot
+ggplot(preds, aes(x = treatment, y = Estimate)) +
   geom_violin(data = preds_full, aes(fill = treatment)) +
-  geom_pointrange(aes(ymin =  Q5, ymax = Q95),color="white") +
+  geom_pointrange(aes(ymin = Q5, ymax = Q95), color = "white") + 
   geom_point(data = shrub, aes(y = pft_biomass, x = treatment)) +
-  theme_classic() +  
-  scale_fill_manual(values = c("Control" = "#6c6563",
-                               "Heat wave" = "#b56d5e",
-                               "Extended season" = "#bbbc81")) 
-
-preds_delta[,1-mean(Estimate<0),by = treatment]
-preds_delta[,mean(Estimate),by = treatment]
-
-ggplot(preds_delta,aes(x = Estimate,fill = treatment ))+
-  geom_vline(xintercept = 0,lty = 2)+
-  geom_density(alpha = 0.5)+
-  theme_classic()+  
+  theme_classic() +
   scale_fill_manual(values = c("Control" = "#6c6563",
                                "Heat wave" = "#b56d5e",
                                "Extended season" = "#bbbc81"))
 
+# how much of the delta control - treatment is positive? 
+  # if more than 90% i'm confident in treatment effect
+preds_delta[,1-mean(Estimate < 0), by = treatment]
+preds_delta[,mean(Estimate), by = treatment]
 
+ggplot(preds_delta, aes(x = Estimate, fill = treatment)) + 
+  geom_vline(xintercept = 0, lty = 2) +
+  geom_density(alpha = 0.5) +
+  theme_classic()+  
+  scale_fill_manual(values = c("Control" = "#6c6563",
+                               "Heat wave" = "#b56d5e",
+                               "Extended season" = "#bbbc81"))+
+  facet_wrap(~treatment,scales = "free_y",nrow = 3)
+
+# distribution of the delta control - treatment, JB doesn't like
 ggplot(preds_delta,aes(x = treatment, y = Estimate ,fill = treatment ))+
   geom_hline(yintercept = 0,lty = 2)+
   geom_violin(alpha = 0.95)+
@@ -166,5 +180,56 @@ ggplot(preds_delta,aes(x = treatment, y = Estimate ,fill = treatment ))+
                                "Heat wave" = "#b56d5e",
                                "Extended season" = "#bbbc81"))
 
-plot(bayesian_model)
-pp_check(bayesian_model,dens_overlay = 100)
+
+
+### GRAMINOID statistical reporting with the fit models ###
+new_data <- data.table(treatment = sort(unique(graminoid$treatment)))
+preds <- cbind(new_data, fitted(bayesian_model_gram,
+                                newdata = new_data, probs = c(0.05, 0.95)))
+
+preds_full <- data.table(fitted(bayesian_model_gram,
+                                newdata = new_data, summary = F))
+colnames(preds_full) <- as.character(new_data$treatment)
+
+preds_delta <- preds_full[,.(`Heat wave` = `Heat wave` - Control ,
+                             `Extended season` =`Extended season` - Control)]
+
+# plot set up
+preds_full <- melt(preds_full, variable.name = "treatment", value.name = "Estimate")
+preds_full$treatment <- as.factor(preds_full$treatment)
+
+preds_delta <- melt(preds_delta, variable.name = "treatment", value.name = "Estimate")
+
+# distribution of the mean plot
+ggplot(preds, aes(x = treatment, y = Estimate)) +
+  geom_violin(data = preds_full, aes(fill = treatment)) +
+  geom_pointrange(aes(ymin = Q5, ymax = Q95), color = "white") + 
+  geom_point(data = graminoid, aes(y = pft_biomass, x = treatment)) +
+  theme_classic() +
+  scale_fill_manual(values = c("Control" = "#6c6563",
+                               "Heat wave" = "#b56d5e",
+                               "Extended season" = "#bbbc81"))
+
+# how much of the delta control - treatment is positive? 
+# if more than 90% i'm confident in treatment effect
+preds_delta[,1-mean(Estimate < 0), by = treatment]
+preds_delta[,mean(Estimate), by = treatment]
+
+ggplot(preds_delta, aes(x = Estimate, fill = treatment)) + 
+  geom_vline(xintercept = 0, lty = 2) +
+  geom_density(alpha = 0.5) +
+  theme_classic()+  
+  scale_fill_manual(values = c("Control" = "#6c6563",
+                               "Heat wave" = "#b56d5e",
+                               "Extended season" = "#bbbc81"))+
+  facet_wrap(~treatment,scales = "free_y",nrow = 3)
+
+# distribution of the delta control - treatment, JB doesn't like
+ggplot(preds_delta,aes(x = treatment, y = Estimate ,fill = treatment ))+
+  geom_hline(yintercept = 0,lty = 2)+
+  geom_violin(alpha = 0.95)+
+  theme_classic()+  
+  scale_fill_manual(values = c("Control" = "#6c6563",
+                               "Heat wave" = "#b56d5e",
+                               "Extended season" = "#bbbc81"))
+
